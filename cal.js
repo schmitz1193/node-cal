27bfb5f0fb705996b113e192e9045c2a8e9021f0
schmitz1193 <schmitz1193@gmail.com>
2016-01-28 10:50:47 -0600

    Jan 2016 passes

--- cal.js
+++ cal.js
@@ -1,5 +1,18 @@
-#!/usr/bin/env node
+#!/usr/bin/env node --harmony_destructuring
 
 'use strict';
 
-console.log('cal');
+//ignore the first two arguments? first two arguments are the month and the year
+//const [,, ...args] = process.argv;
+
+console.log('args');
+
+//if (args.length === 2) {
+//  const [month, year] = args;
+//  console.log(`generateMonth(${year},${month})`;
+//} else if(args.length === 1) {
+//  const [year] = args;
+//  console.log(`generateYear(${year})`)
+//} else {
+//  console.log('broke it');
+//}
--- tests/test.js
+++ tests/test.js
@@ -41,21 +41,40 @@ describe('cal', () => {
 
     describe('.getDay', () => {
       it('returns 2 (Tuesday) for March 1, 2016', () => {
-        expect(zellers.getDay(2016, 3, 1)).to.equal(2);
+        expect(zellers.modDay(2016, 3, 1)).to.equal(2);
       });
       it('returns 3 (Wednesday) for March 1, 2000', () => {
-        expect(zellers.getDay(2000, 3, 1)).to.equal(3);
+        expect(zellers.modDay(2000, 3, 1)).to.equal(3);
       });
       it('returns 1 (Monday) for March 1, 2100', () => {
-        expect(zellers.getDay(2100, 3, 1)).to.equal(1);
+        expect(zellers.modDay(2100, 3, 1)).to.equal(1);
       });
       it('returns 0 (Sunday) for March 1, 2200', () => {
-        expect(zellers.getDay(2200, 3, 2)).to.equal(0);
+        expect(zellers.modDay(2200, 3, 2)).to.equal(0);
       });
       it('returns 4 (Thursday) for March 1, 2300', () => {
-        expect(zellers.getDay(2300, 3, 1)).to.equal(4);
+        expect(zellers.modDay(2300, 3, 1)).to.equal(4);
       });
     });
   });
-});
+  describe('the month output', () => {
+    const month = require('../lib/month.js');
+      it('should handle January', () => {
+        expect(month.setUpMonth(2016, 1)).to.equal('    January 2016');
+      });
+  });
+  describe('the weekday output', () => {
+    const month = require('../lib/month.js');
+      it('should print days with spaces', () => {
+        expect(month.setUpWeekDays(2016, 1)).to.equal(`Su Mo Tu We Th Fr Sa\n`);
+      });
+  });
+  describe('the dates with 31 days', () => {
+    const month = require('../lib/month.js');
+     it('should print all the dates in January',() => {
+        expect(month.setUpDates(2016, 1, 1)).to.equal(`                1  2\n 3  4  5  6  7  8  9\n10 11 12 13 14 15 16\n17 18 19 20 21 22 23\n24 25 26 27 28 29 30\n31\n`);
+      });
+  });
 
+
+});
